0befdf12c677b0bc12dfaa83522f8dbbb2ce945b
pimparwar <pimparwar.p@northeastern.edu>
2021-04-29 14:12:05 -0400

    adding admin actions can view room detais in user booked for

--- ADMIN_ACTIONS.sql
+++ ADMIN_ACTIONS.sql
@@ -26,9 +26,31 @@ GROUP BY
 ORDER BY
     rb.booking_id;
 
+-- detailed booking history with room numbers 
+    
+SELECT
+    rb.booking_id,
+    ue.first_name || ' ' ||
+    ue.last_name as name,
+    h.hotel_name,h.address,
+    c.city_name,
+    rb.room_id As room_number
+FROM
+         room_booked_entity rb
+    JOIN booking      b ON rb.booking_id = b.booking_id
+    JOIN user_entity  ue ON b.user_id = ue.user_id
+    JOIN hotel        h ON b.hotel_id = h.hotel_id
+    JOIN CITY c ON h.city_id = c.city_id
+ORDER BY
+    rb.booking_id;    
+
+-- adding hotels
+
 begin
 admin.Add_HOTEL('Marriot',5,400,501,7898989898,'11 AM', '12 PM', 'NEW YORK CITY');
 End;
+
+-- adding rooms
 
 begin
 admin.ADD_ROOM(101,306);
