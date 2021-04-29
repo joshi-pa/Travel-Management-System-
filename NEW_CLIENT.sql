3add11b7ca762e40a60d7cf27a0da599dbe3396e
joshi-pa <joshi.pa@northeastern.edu>
2021-04-29 03:42:08 -0400

    Signup procedure user_action

---
+++ NEW_CLIENT.sql
@@ -0,0 +1,25 @@
+SET SERVEROUTPUT ON;
+
+BEGIN
+admin.help;
+END;
+
+BEGIN
+admin.signup('sachin','Sachin','Tendulkar','sach1in@gmail.com','Cricket123456',7894561455,1,500,'26-oct-1964');
+END;
+
+BEGIN
+admin.signup('brett','Brett','Lee','Brett@gmail.com','Batsman123456',1020125410,2,501,'26-oct-1967');
+END;
+
+BEGIN
+admin.signup('RICKY','RICKY','PONTING','ricky@gmail.com','Captain123456',11100221452,3,502,'06-DEC-1981');
+END;
+
+BEGIN
+admin.signup('irfan','Irfan','Pathan','irfu@gmail.com','Bowler123456',4141525263,3,502,'26-NOV-1984');
+END;
+
+BEGIN
+admin.signup('Daniel','Daniel','Craig','daniel@gmail.com','Daniel1234567',7411114447,3,502,'7-MAY-1984');
+END;
