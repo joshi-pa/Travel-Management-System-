c277d9399be8be098a8a27ee237fded1cdd6c9dc
DubeyDipti <dubey.di@northeastern.edu>
2021-04-29 04:10:55 -0400

    Added code in Book a room & User_action_hotel_search

--- USER_ACTION_BOOKROOM.sql
+++ USER_ACTION_BOOKROOM.sql
@@ -6,4 +6,11 @@ INNER JOIN ADMIN.room r ON r.hotel_id = h.hotel_id
 INNER JOIN ADMIN.room_type rt ON r.room_type_id = rt.room_type_id
 WHERE r.room_id NOT IN (SELECT room_booked_id FROM ADMIN.room_booked_entity);
 
+-----3. VIEW BOOKING AND PAYMENT STATUS----------------
+ SELECT B.BOOKING_ID, B.USER_ID, B.TOTAL_ROOMS_BOOKED, B.TOTAL_AMOUNT,B.PAYMENT_ID, BS.BOOKING_STATUS
+FROM BOOKING B
+JOIN PAYMENT P ON B.PAYMENT_ID = P.PAYMENT_ID
+JOIN PAYMENT_TYPE PT ON p.payment_type_id= pt.payment_type_id
+JOIN BOOKING_STATUS BS ON B.BOOKING_STATUS_ID= bs.booking_status_id;
+
 
